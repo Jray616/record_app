c57dc1849a8bda96bd0056ec1810c15c178430c4
JR <624397346@qq.com>
2019-03-05 18:02:52 +0800

    新增基础页面

--- lib/src/pages/record.dart
+++ lib/src/pages/record.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class Record extends StatelessWidget{
@@ -25,7 +27,7 @@ class Record extends StatelessWidget{
               children: <Widget>[
                 Center(
                   child: Container(
-                    height: 200.0,
+                    margin: const EdgeInsets.all(10.0),
                     child: IconList()
                   ),
                 ),
@@ -41,27 +43,80 @@ class Record extends StatelessWidget{
 
 }
 
+
+//图标列表组件
 class IconList extends StatelessWidget{
 
-  final List<String> items;
+  final List<Map> items;
   IconList({Key key,@required this.items}):super(key:key);
+
+  //获取图标列表
+  Future getIconsList() async{
+    Dio dio = new Dio();
+    var url = "http://127.0.0.1:8081/directory/getAllDirectory";
+
+    Response response = await dio.post(url);
+    String data = response.data;
+    Map map = json.decode(data);
+    return map;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    var iconsList = getIconsList();
+
+    // TODO: implement build
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          mainAxisSpacing:5.0,  //纵轴间距
+          crossAxisSpacing: 5.0,
+          childAspectRatio: 1
+      ),
+      itemCount: items.length,
+      itemBuilder: (BuildContext context,int index){
+        Map map = items[index];
+        String url = map['url'];
+        String text = map['text'];
+        return icon(url: url,text: text);
+      },
+    )
+    ;
+  }
+
+
+
+}
+
+
+//图标组件
+class icon extends StatelessWidget{
+
+  final String url;
+  final String text;
+
+  const icon({Key key, this.url, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return GridView(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing:2.0,  //纵轴间距
-          crossAxisSpacing: 2.0,
-          childAspectRatio: 1
-      ),
+    return Column(
       children: <Widget>[
-
-
+        Image.asset(url,
+          width: 50.0,
+          height: 50.0,
+          fit: BoxFit.contain,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(text)
+          ],
+        ),
       ],
-    )
-    ;
+    );
   }
 
 }
